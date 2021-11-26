baf576043773a016303b2bb4c28c2482165b6231
nucleosp <91513343+nucleosp@users.noreply.github.com>
2021-11-26 14:49:48 -0300

    Add files via upload

---
+++ src/pages/ejtecnologia.js
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import EJ from "../components/EJ/index";
+import EJ1 from "../components/EJ1/index";
+import EJ2 from "../components/EJ2/index";
+
+export default function EjsTecnologia() {
+  return (
+    <div>
+      <EJ2
+        name="CORE Júnior"
+        imgPath="LogosEJ/Tecnologia/CORE.jpg"
+        cidade="Santos."
+        universidade="USP."
+        curso="Engenharia Mecânica, de Produção,
+        de Petróleo, Química e Civil."
+        serviços="Desenvolvimento de aplicativos,
+        desenvolvimento de sites, criação de planilhas
+        em Excel/VBA, desenvolvimento de projetos
+        piloto em outras linguagens de programação."
+      ></EJ2>
+      <EJ
+        name="Gamma JR."
+        imgPath="LogosEJ/Tecnologia/gamma.jpg"
+        cidade="Lorena."
+        universidade="USP."
+        curso="Engenharia Física."
+        serviços="Automação residencial e Desenvolvimento
+        de sites e aplicativos."
+      ></EJ>
+      <EJ1
+        name="ITA Júnior"
+        imgPath="LogosEJ/Tecnologia/ITA.jpg"
+        cidade="São José dos Campos."
+        universidade="ITA."
+        curso="Engenharia Aeronáutica, Eletrônica,
+        Mecânica-Aeronáutica, Civil-Aeronáutica,
+        Computação e Aeroespacial."
+        serviços="Tecnologia da informação
+        (desenvolvimento de sites e aplicativos),
+        Desenvolvimento de automatizações, Criação de
+        planilhas e dashboards, Pesquisa de mercado e
+        Pesquisa operacional."
+      ></EJ1>
+      <EJ
+        name="Síntese JR"
+        imgPath="LogosEJ/Tecnologia/SINTESE.png"
+        cidade="São Paulo."
+        universidade="USP."
+        curso="Sistemas de Informação."
+        serviços="Desenvolvimento de sistemas web
+        para pequenas empresas, Site e-commerce,
+        Site institucional, Desenvolvimento de
+        aplicativos mobile, Design de interface para
+        sites e aplicativos e Sistemas de agendamento."
+      ></EJ>
+    </div>
+  );
+}
---
+++ src/pages/services.js
@@ -0,0 +1,131 @@
+import { StylesProvider } from "@material-ui/core";
+import { useState } from "react";
+import styles from "../styles/services.module.css";
+
+import Link from "next/link";
+
+export default function Services() {
+  return (
+    <div className={styles.servicesContent}>
+      <div className={styles.pageTitleWrap}>
+        <div className={styles.pageTitle}>
+          <h1 style={{ marginLeft: "20%" }}>SERVIÇOS</h1>
+        </div>
+      </div>
+      <div className={styles.allWidth}>
+        <div className={styles.servicesTexts}>
+          <div className={styles.subtitle}>
+            <h2>COMO UMA EMPRESA JÚNIOR PODE TE AJUDAR?</h2>
+          </div>
+          <p>
+            Uma empresa júnior é uma organização sem fins lucrativos, formada e
+            gerida por{" "}
+            <span className={styles.coloredTitle}>
+              estudantes universitários voluntários
+            </span>{" "}
+            de diversos cursos que buscam colocar em prática os conhecimentos
+            adquiridos em salas de aula por meio da vivência empresarial e da
+            realização de projetos reais. Vale ressaltar que tais estudantes
+            estão sob a orientação de profissionais atuantes na área. Além
+            disso, uma vez que estas empresas não possuem fins lucrativos, todo
+            o dinheiro arrecadado é investido na própria empresa e na
+            capacitação dos membros.
+          </p>
+          <p>
+            Atualmente, o Brasil é o país com maior número de empresas júniores
+            no mundo, apresentando um movimento que cresce exponencialmente a
+            cada ano, impactando o mercado nacional e formando cada vez mais
+            empresários capazes e comprometidos a transformar o Brasil.
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.allWidth}>
+        <div className={styles.servicesTexts}>
+          <div className={styles.subtitle}>
+            <h2>TE CONECTAMOS COM A SOLUÇÃO</h2>
+          </div>
+          <p>
+            As empresas juniores são capazes de oferecer um{" "}
+            <span className={styles.coloredTitle}>
+              atendimento personalizado{" "}
+            </span>{" "}
+            e completo para o seu negócio. E isso é muito importante, pois a
+            qualidade dos serviços prestados por outras empresas ao seu negócio
+            tem impacto direto sobre a qualidade na oferta dos seus produtos e
+            serviços ao seu cliente. Este atendimento mais próximo é possível 
+            graças à estrutura e aos objetivos particulares de uma empresa júnior.
+          </p>
+          <p>
+            Com isso, o acompanhamento do seu projeto será de perto em todos os
+            estágios, por uma equipe engajada e{" "}
+            <span CLASSnAME={styles.coloredTitle}>
+              {" "}
+              alinhada com suas metas{" "}
+            </span>
+            e necessidades específicas. Desta forma, o serviço prestado tende a
+            ser mais satisfatório e oferecer resultados mais condizentes com as
+            expectativas do seu negócio.
+          </p>
+        </div>
+      </div>
+      <div className={styles.allWidth}>
+        <div className={styles.servicesTexts}>
+          <div className={styles.subtitle}>
+            <h2>EXEMPLOS DE AREAS E EMPRESAS</h2>
+          </div>
+          <div>
+          </div>
+          <div>
+            <button className={styles.button}>
+              <Link href="/ejtecnologia" className={styles.link}>
+                <a>Tecnologia</a>
+              </Link>
+            </button>
+            <button className={styles.button}>
+              <Link href="/ejarquitetura" className={styles.link}>
+                <a>Arquitetura</a>
+              </Link>
+            </button>
+            <button className={styles.button}>
+              <Link href="/ejciencia" className={styles.link}>
+                <a>Ciência</a>
+              </Link>
+            </button>
+            <button className={styles.button}>
+              <Link href="/ejcomunicacao" className={styles.link}>
+                <a>Comunicação</a>
+              </Link>
+            </button>
+            <button className={styles.button}>
+              <Link href="/ejdireito" className={styles.link}>
+                <a>Direito</a>
+              </Link>
+            </button>
+            <button className={styles.button}>
+              <Link href="/ejengenharia" className={styles.link}>
+                <a>Engenharia</a>
+              </Link>
+            </button>
+            <button className={styles.button}>
+              <Link href="/ejgestao" className={styles.link}>
+                <a>Gestão</a>
+              </Link>
+            </button>
+            <button className={styles.button}>
+              <Link href="/ejrelacoesinternacionais" className={styles.link}>
+                <a>Relações Internacionais</a>
+              </Link>
+            </button>
+            <button className={styles.button}>
+              <Link href="/ejsaude" className={styles.link}>
+                <a>Saúde</a>
+              </Link>
+            </button>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+}
